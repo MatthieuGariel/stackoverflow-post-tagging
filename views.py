fa025e433f4e0425959ed05ab9639a8dfc17f14c
Matthieu Gariel <gariel.matthieu@hotmail.fr>
2021-01-28 05:02:21 +0100

    result route tested

--- views.py
+++ views.py
@@ -32,20 +32,19 @@ def index(): return render_template('index.html')
 @app.route('/result', methods=['GET', 'POST'])
 def result():
     if request.method == 'POST':
-        title = request.args.get('post_title')
-        body = request.args.get('post_text')
-        #result = request.form
+        result = request.form
+        title = result['post_title']
+        body = result['post_text']
         titleAndBody = str(title) + " " + str(body)
         corpus = word_tokenize(titleAndBody)
         corpus = [word.lower() for word in corpus]
-        print("corpus")
-        #corpus = corpus.lower()
         corpus_wo_stopwords = list(filter(lambda x: x not in stop, corpus))
-        corpus_wo_stopwords = [i for i in corpus_wo_stopwords if not i.isalpha()]
+        corpus_wo_stopwords = [i for i in corpus_wo_stopwords if i.isalpha()]
         corpus_wo_stopwords = [word.lower() for word in corpus_wo_stopwords]
         corpus_lanc_stemmed = [stemmer.stem(word) for word in corpus_wo_stopwords]
         corpus_lanc_stemmed = ' '.join(corpus_lanc_stemmed)
         # corpus_lanc_stemmed_transformed = scaler_imported.transform(corpus_lanc_stemmed)
+        # print(corpus_lanc_stemmed)
         return (str(corpus_lanc_stemmed))
     # else:
     #    return (request.form)
